31fe3824cbcbefc28828b8e1b0f6c12bda8ffee6
Gregory <lonsdale.g@gmail.com>
2017-09-22 13:08:02 -0400

    test commit

--- DebateApp/DebateApp.Client/Startup.cs
+++ DebateApp/DebateApp.Client/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+//thing
 namespace DebateApp.Client
 {
     public class Startup
