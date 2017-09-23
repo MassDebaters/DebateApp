c5a549783f4e49ab6edf573a2e6de612b0dcc5cc
Gregory <lonsdale.g@gmail.com>
2017-09-23 12:20:16 -0400

    Added project structure

---
+++ DebateApp/DebateApp.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DebateApp.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ DebateApp/DebateApp.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DebateApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
