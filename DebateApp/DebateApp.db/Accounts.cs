e43b2a709c9d40a1bea1b6592ef4715c4ad05e19
Lenny Lopez <lenny.lopez@live.com>
2017-09-24 13:55:14 -0400

    Added scaffolded classes to db project

---
+++ DebateApp/DebateApp.db/Accounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebateApp.db
+{
+    public partial class Accounts
+    {
+        public Accounts()
+        {
+            Posts = new HashSet<Posts>();
+        }
+
+        public int AccountId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Astros { get; set; }
+
+        public ICollection<Posts> Posts { get; set; }
+    }
+}
