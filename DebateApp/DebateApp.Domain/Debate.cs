662ffcd6c7f9b03ad528c5fbdd7fa56edc051f83
greggy <lonsdale.g@gmail.com>
2017-09-26 21:36:11 -0400

    deleted commented out code in debate.cs

--- DebateApp/DebateApp.Domain/Debate.cs
+++ DebateApp/DebateApp.Domain/Debate.cs
@@ -9,11 +9,7 @@ namespace DebateApp.Domain
 {
     public abstract class Debate
     {
-        //private List<JObject> jsonObjects = new List<JObject>();
-        //private List<string> DebateStrings = new List<string>();
-        ////private StreamWriter FileOut = new StreamWriter("DebateStrings.txt");
-        //private StreamReader FileIn;
-        //private List<Debate> dList = new List<Debate>();
+
 
         public int DebateID { get; set; }
         public List<Post> Posts { get; set; }
@@ -29,63 +25,13 @@ namespace DebateApp.Domain
         public int NumberOfRounds { get; set; }
         public int Pot { get; set; }
 
-        /*public Casual GetDebate(int id)
-        {
-            var stringId = id.ToString();
-
-            ConvertDebatesToJsonObj();
-
-            JObject debate = jsonObjects.Values<JObject>()
-                .Where(m => m["DebateId"].Value<string>() == stringId)
-                .FirstOrDefault();
-
-            var debateString = debate.ToString();
-
-            Casual debateObject = JsonConvert.DeserializeObject<Casual>(debateString);
-
-            jsonObjects.Clear();
-
-            return debateObject;
-        }*/
-
-        //public void ConvertDebatesToJsonObj()
-        //{
-        //    string line;
-
-        //    while ((line = FileIn.ReadLine()) != null)
-        //    {
-        //        JObject jo = JObject.Parse(line);
-        //        jsonObjects.Add(jo);
-        //    }
-        //    FileIn.Close();
-        //}
-
-        //public void DeleteDebate(int id)
-        //{
-        //    //read file and store to string list
-        //    //look through string list and delete the one with id
-        //    //
-        //}
-
-        //public void UpdateDebate(int id, List<Post> p)
-        //{
-
-        //    dList = JsonConvert.DeserializeObject<List<Debate>>(FileIn.ReadToEnd());
-
-        //    Debate d2 = dList.Find(d => d.DebateID == id);
-
-        //    d2.Posts = p;
-
-        //    var newInfo = JsonConvert.SerializeObject(dList);
-
-        //    File.WriteAllText("DebateStrings.txt", newInfo);
-        //}
-        public string message;
-        public void AddDebate(Casual d)
+     
+        
+        public string SaveDebate(Casual d)
         {
             var path = "C:\\Revature\\DebateApp\\DebateApp\\DebateApp.Domain\\DebateStrings.txt";
             string s = File.ReadAllText(path);
-            message = s;
+            
             var DebateList = JsonConvert.DeserializeObject<List<Casual>>(s);
 
             try
@@ -97,9 +43,10 @@ namespace DebateApp.Domain
                 DebateList = new List<Casual>();
                 DebateList.Add(d);
             }
-
+            
             var NewList = JsonConvert.SerializeObject(DebateList);
             File.WriteAllText(path, NewList);
+            return s;
             
         }
     }
