3b3b08edb14f70eb0c374f672d205fb1d07a7599
Gregory <lonsdale.g@gmail.com>
2017-09-28 19:42:03 -0400

    made debatemodel class

---
+++ DebateAppDomain/DebateAppDomainAPI/Models/DebateModel.cs
@@ -0,0 +1,45 @@
+﻿using DebateApp.Domain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DebateAppDomainAPI.Models
+{
+    public class DebateModel
+    {
+        private int _D_id;
+        private int _U_id;
+        private HttpClient client = new HttpClient();
+        private string DBRest = "http://localhost:54277/api/";
+        public DebateModel(int D_id, int U_id)
+        {
+            _D_id = D_id;
+            _U_id = U_id;
+        }
+        private User _u
+        {
+            get
+            {
+                var res = client.GetAsync(DBRest + "/User/Get")
+            }
+        }
+        private Debate _d { get
+            {
+                try
+                {
+                    var res = client.GetAsync(DBRest + "Debate/Get/" + _D_id).GetAwaiter().GetResult();
+                    var ResObject = JsonConvert.DeserializeObject<Debate>(res.ToString());
+                    return ResObject;
+                }
+                catch(Exception)
+                {
+                    return new Casual(_u.UserID);
+                }
+            }
+
+        
+    }
+}
