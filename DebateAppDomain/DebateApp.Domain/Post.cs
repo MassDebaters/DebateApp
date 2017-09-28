e49fdbbfcaec5c130c013848032cacb8d34951d7
greggy <lonsdale.g@gmail.com>
2017-09-27 20:45:21 -0400

    Added validate method to Post class

--- DebateAppDomain/DebateApp.Domain/Post.cs
+++ DebateAppDomain/DebateApp.Domain/Post.cs
@@ -6,11 +6,21 @@ namespace DebateApp.Domain
 {
     public abstract class Post
     {
+        private static int idcounter = 0;//change this to pull from a persistent id counter later
         public int PostID { get; set; }
         public string CommentText { get; set; }
         public DateTime TimeStamp { get; set; }
         public int UserID { get; set; }
         public int MaxLength { get; set; }
 
+        public Post()
+        {
+            PostID = idcounter;
+            idcounter += 1;
+        }
+        public virtual bool Validate()
+        {
+            return CommentText.Length <= MaxLength;
+        }
     }
 }
