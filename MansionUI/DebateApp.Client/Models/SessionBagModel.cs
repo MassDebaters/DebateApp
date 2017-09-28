bb2449952d208864c43962887651dc5ea8a5b9e4
Madison <madisononealburke@gmail.com>
2017-09-27 22:24:09 -0400

    Removed Dependencies

--- MansionUI/DebateApp.Client/Models/SessionBagModel.cs
+++ MansionUI/DebateApp.Client/Models/SessionBagModel.cs
@@ -1,12 +1,11 @@
 using System;
-using DebateApp.Domain;
 using System.Collections.Generic;
 
 namespace DebateApp.Client.Models
 {
     public class SessionBagModel
     {
-        public User User { get; set; }
-        public List<Debate> Debates {get; set;}
+        public UserModel UserModel { get; set; }
+        //public List<Debate> Debates {get; set;}
     }
 }
