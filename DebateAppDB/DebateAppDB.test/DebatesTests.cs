251343f4be93e1bfbcb6cd308c4a923752daabff
Lenny Lopez <lenny.lopez@live.com>
2017-10-10 16:05:40 -0400

    Added more debate tests, fixed some methods

--- DebateAppDB/DebateAppDB.test/DebatesTests.cs
+++ DebateAppDB/DebateAppDB.test/DebatesTests.cs
@@ -33,12 +33,26 @@ namespace DebateAppDB.test
 
             var ModelToJson = JsonConvert.SerializeObject(debates);
             var JsonObject = JObject.Parse(ModelToJson);
-            var actual = JsonObject.SelectToken(@"d.debateTopic");//.Value<string>();
+            var actual = JsonObject.SelectToken(@"d.debateTopic").Value<string>();
             var expected = "this is a debate";
 
             Assert.NotNull(debates);
             Assert.IsType<DebateModel>(debates);
             Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void AddDebates()
+        {
+            var newDebate = "{\"d\":{\"teams\":[{\"members\":[{\"hasVoted\":false,\"hasResponded\":false,\"userID\":70,\"username\":\"testgirl5\",\"password\":null,\"astros\":100}],\"roundsWon\":0,\"winningsShare\":0.5,\"opener\":{\"commentText\":\"lyes\",\"timeStamp\":\"10 / 10 / 2017 3:54:50 PM\",\"userID\":70},\"teamLimit\":1,\"readyToStart\":true},{\"members\":[],\"roundsWon\":0,\"winningsShare\":0.5,\"opener\":null,\"teamLimit\":1,\"readyToStart\":false}],\"round\":[],\"pot\":10,\"debate_ID\":1,\"_gamestage\":false,\"setupStage\":true,\"completed\":false,\"awardsDisbursed\":false,\"debateTopic\":\"this is a debate\",\"debateCategory\":\"debates\",\"audience\":[],\"turnLength\":60,\"postLength\":200,\"sourcesRequired\":0,\"numberOfRounds\":5,\"currentPotShareL\":0.0,\"currentPotShareR\":0.0,\"status\":\"Setup Stage.\",\"roundStart\":0,\"lastGet\":0,\"winner\":null,\"loser\":null}}";
+            var Debate = JsonConvert.DeserializeObject<DebateModel>(newDebate);
+            Assert.NotNull(Debate);
+            debate.AddDebate(Debate);
+
+            var actual = debate.GetDebate(4);
+
+            Assert.NotNull(actual.d);
+            Assert.IsType<DebateModel>(actual);
         }
     }
 }
